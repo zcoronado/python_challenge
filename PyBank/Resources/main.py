f87b9d0bdd163933466781574a93f742d548fdd5
Zaira Coronado <coronado.zaira@gmail.com>
2021-04-16 21:13:20 -0700

    Python homework

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -14,36 +14,34 @@ with open(filepath, 'r') as open_file:
     dates = []
     profits = []
     profit_changes = []
+    
 
     for row in csv_reader: 
         dates.append(row[0])
         if len(profits) > 0: 
             profit_changes.append(int(row[1])-profits[-1])
         profits.append(int(row[1]))
-        
+  
+    
 
         #profit_loses = int(if budget_data[1] < 0)
        # profit_wins = int(if budget_data[1] > 0)
 # The total number of months included in the dataset
     unique_date = len(dates) 
-    print(unique_date)
-
 
 # # The net total amount of "Profit/Losses" over the entire period
-
     total_net  =  sum(profits)
-    print(total_net)
-
 
 # # Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
     average_profits = round(statistics.mean(profit_changes),2)
-    print(average_profits)
 
 # # The greatest increase in profits (date and amount) over the entire period
-#     greatest_increase = profit_wins.max()
+    greatest_increase = max(profit_changes)
+    max_increase_date = dates[profit_changes.index(greatest_increase)]
 
 # # The greatest decrease in losses (date and amount) over the entire period
-#     greatest_decrease = profit_wins.min()
+    greatest_decrease = min(profit_changes)
+    min_increase_date = dates[profit_changes.index(greatest_decrease)]
 
 # # As an example, your analysis should look similar to the one below:
 
@@ -56,15 +54,17 @@ with open(filepath, 'r') as open_file:
 # #  Greatest Increase in Profits: Feb-2012 ($1926159)
 # #  Greatest Decrease in Profits: Sep-2013 ($-2196167)
 # #  ```
-#      print(f'''
-#         Financial Analysis
-#         ===============================
-#         Total Months : {round(unique_date, 2)}
-#         Total :  {round(total_net, 2)}
-#         Average  Change: ${round(, 2 )}
-#         Greatest Increase in Profits: {greatest_increase}
-#         Greatest Decrease in Profits: {greatest_decrease}
-#     ''')
+    print(f'''
+        Financial Analysis
+        ===============================
+        Total Months : {round(unique_date, 2)}
+        Total :  {round(total_net, 2)}
+        Average  Change: ${round(average_profits, 2 )}
+        Greatest Increase in Profits: {max_increase_date }, (${greatest_increase})
+        Greatest Decrease in Profits: {min_increase_date }, (${greatest_decrease})
+     ''')
 
 
-#  In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+#  In addition, your final script should both print the analysis to the terminal 
+#  and export a text file with the results.
+    
