815ada4e79f7159085ce363e336c20f9da06208e
Zaira Coronado <coronado.zaira@gmail.com>
2021-04-17 23:05:08 -0700

    PyPoll Progress

--- PyPoll/Resources/main.py
+++ PyPoll/Resources/main.py
@@ -7,22 +7,18 @@ import numpy as np
 # You will be give a set of poll data called election_data.csv. The dataset is composed of three columns: Voter ID, County, and Candidate. Your task is to create a Python script that analyzes the votes and calculates each of the following:
 filepath = os.path.join("election_data.csv")
 with open(filepath, 'r') as open_file: 
-    csv_reader = csv.reader(open_file)
+    csv_reader = csv.reader(open_file,delimiter=",")
 
     next(csv_reader)
 
-    voter_id = []
-    voter_county = []
     voter_candidate = []
-    unique_candidates = []
+    # unique_candidates = []
     
     cand_votes={}
     cand_opt=[]
     total_votes = 0 
 
     for row in csv_reader: 
-        voter_id.append(row[0])
-        voter_county.append(row[1])
         voter_candidate.append(row[2])
         total_votes = total_votes + 1 
 
@@ -33,54 +29,14 @@ with open(filepath, 'r') as open_file:
         
         cand_votes[cand_name] += 1 
    
-print(cand_votes)
+# print(cand_votes)
 
 for cand in cand_votes:
     x = cand_votes.get(cand)
-    per_x = float(x)/float(total_votes) *100
-    print(per_x)
-
-# The total number of votes cast
-
-# A complete list of candidates who received votes
-
-
-
-    # unique_candidates = np.unique(voter_candidate)
-    # print(unique_candidates)
-
-# The percentage of votes each candidate won 
-    # for row in csv_reader: 
-    #     total_votes += 1
-        
-    #     if voter_candidate in unique_candidates:
-    #         candidate_index = unique_candidates.index(voter_candidate)
-    #         candidate_vote_count[candidate_index] = candidate_vote_count[candidate_index] + 1
-    #     else:
-    #         #if candidate was not found in candidates_unique list then append to list and add 1 to vote count
-    #         candidates_unique.append(voter_candidate)
-    #         candidate_vote_count.append(1)
-
-
-#     khan_votes = "Khan"
-#     khan_total_votes = len("Khan")
-#     khan_percent = khan_total_votes/(total_votes)
-#     print(khan_percent)
-    
-
-
-# # The total number of votes each candidate won
-#     count_dictionary = {}
-#     for name in voter_candidate:
-#         if name in count_dictionary:
-#             count_dictionary[name] +=1 
-#         else:
-#             count_dictionary[name] = 1
-#     print(count_dictionary)
-
-    
-        
-# # The winner of the election based on popular vote.
+    per_x = round(int(x)/int(total_votes) *100 , 2)
+    # print(per_x,)
+    # print(x)
+ 
 
 # # As an example, your analysis should look similar to the one below:
 
@@ -95,4 +51,23 @@ for cand in cand_votes:
 # # -------------------------
 # # Winner: Khan
 # # -------------------------
-# # In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+print(f'''
+        "Election Results"
+        "==============================="
+        "Total Votes: {total_votes}"
+        "==============================="
+        ''')
+
+#Could not get the output to work 
+
+# for i in (cand_votes):
+#     print(f'''
+#     "{cand[i]} : {int(per_x[i])} , % {int(x[i])} "
+#     ''')
+
+output = os.path.join("..", "analysis",  'analysis.txt')
+with open(output,"w") as new:
+    new.write("Election Results")
+    new.write("===============================")
+    new.write(f"Total Votes: {total_votes}")
+    new.write("===============================")
