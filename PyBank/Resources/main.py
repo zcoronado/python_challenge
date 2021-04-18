86889c739283ebf91bad521fcc3ab7f8fa60ce8d
Zaira Coronado <coronado.zaira@gmail.com>
2021-04-17 17:13:26 -0700

    PyBank Hw Complete

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -5,7 +5,7 @@ import csv
 import statistics 
 
 
-filepath = os.path.join("..", 'Resources', 'budget_data.csv')
+filepath = os.path.join('budget_data.csv')
 with open(filepath, 'r') as open_file: 
     csv_reader = csv.reader(open_file)
 
@@ -37,11 +37,11 @@ with open(filepath, 'r') as open_file:
 
 # # The greatest increase in profits (date and amount) over the entire period
     greatest_increase = max(profit_changes)
-    max_increase_date = dates[profit_changes.index(greatest_increase)]
+    max_increase_date = dates[profit_changes.index(greatest_increase)+1]
 
 # # The greatest decrease in losses (date and amount) over the entire period
     greatest_decrease = min(profit_changes)
-    min_increase_date = dates[profit_changes.index(greatest_decrease)]
+    min_increase_date = dates[profit_changes.index(greatest_decrease)+1]
 
 # # As an example, your analysis should look similar to the one below:
 
@@ -63,23 +63,14 @@ with open(filepath, 'r') as open_file:
         Greatest Increase in Profits: {max_increase_date }, (${greatest_increase})
         Greatest Decrease in Profits: {min_increase_date }, (${greatest_decrease})
      ''')
-
-
-#  In addition, your final script should both print the analysis to the terminal 
-#  and export a text file with the results.
+output = os.path.join("..", "analysis",  'analysis.txt')
+with open(output,"w") as new:
    
-myText = open(r'C:\Users\coron\OneDrive\Desktop\python_challenge\PyBank\analysis','w')
-myString = 
-'T  
-print(f'''
-Financial Analysis
-===============================
-Total Months : {round(unique_date, 2)}
-Total :  {round(total_net, 2)}
-Average  Change: ${round(average_profits, 2 )}
-Greatest Increase in Profits: {max_increase_date }, (${greatest_increase})
-Greatest Decrease in Profits: {min_increase_date }, (${greatest_decrease})
-     ''')'
-myText.write(myString)
-myText.close()
-    
+        new.write("Financial Analysis")
+        new.write("===============================")
+        new.write(f"Total Months : {round(unique_date, 2)}")
+        new.write(f"Total :  {round(total_net, 2)}")
+        new.write(f"Average  Change: ${round(average_profits, 2 )}")
+        new.write(f"Greatest Increase in Profits: {max_increase_date }, (${greatest_increase})")
+        new.write(f"Greatest Decrease in Profits: {min_increase_date }, (${greatest_decrease})")
+   
